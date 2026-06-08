cf0cf9783ebbec01d12ab45c5298a04edee5ede0
g4lb <galevgi@gmail.com>
2026-06-08 07:02:07 +0300

    grove 0.2.0 (autonomous superpowers)

--- Formula/grove.rb
+++ Formula/grove.rb
@@ -3,27 +3,27 @@
 class Grove < Formula
   desc "Orchestrates AI-driven development in isolated environments"
   homepage "https://github.com/g4lb/grove"
-  version "0.1.2"
+  version "0.2.0"
 
   on_macos do
     on_arm do
       url "https://github.com/g4lb/grove/releases/download/v#{version}/grove-darwin-arm64"
-      sha256 "580242f54ba900ce25e9428aecd7028598a19bdf1867940c519672e82a44116a"
+      sha256 "f8367fb69ccf0a30cf4e15024ec5522fdae51303ac9bf6e01227760faddaf105"
     end
     on_intel do
       url "https://github.com/g4lb/grove/releases/download/v#{version}/grove-darwin-x64"
-      sha256 "959bf9c1350d16592d2392f76fd3428fd20cb7b6ce553da4f22678d422af99ce"
+      sha256 "20019ab5fab569ff91df0ecba1cf39dde7b8ee8392dabf45445a06733587a621"
     end
   end
 
   on_linux do
     on_arm do
       url "https://github.com/g4lb/grove/releases/download/v#{version}/grove-linux-arm64"
-      sha256 "8d974fc98dae7fd2645af2766f78bdcd2b079f52c69ee3e5cb57b264268440a8"
+      sha256 "6aec3a91c56c6cc625f479ec57b0d1b1d9d5443e328f417a104fef4a68f57d16"
     end
     on_intel do
       url "https://github.com/g4lb/grove/releases/download/v#{version}/grove-linux-x64"
-      sha256 "193388781bc90c656bc1472bbf5b18b7270117d4bf3ccfe4cb7c20a444e956b8"
+      sha256 "edc81a8ad987e64fed58b0a60af3095965a76baeaa8cd0b5ddbb68d5d48228b1"
     end
   end
 
